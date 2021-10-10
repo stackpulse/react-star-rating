59aa55c94dca2f389c19b883683c3f851fc6cb0a
Spikatrix <12792882+Spikatrix@users.noreply.github.com>
2021-10-10 14:26:07 +0530

    Fixed width icons for the example code

--- example/index.js
+++ example/index.js
@@ -16,9 +16,9 @@ const secondExample = {
   value: 7.5,
   a11y: true,
   isHalf: true,
-  emptyIcon: <i className="far fa-star" />,
-  halfIcon: <i className="fa fa-star-half-alt" />,
-  filledIcon: <i className="fa fa-star" />,
+  emptyIcon: <i className="far fa-fw fa-star" />,
+  halfIcon: <i className="fa fa-fw fa-star-half-alt" />,
+  filledIcon: <i className="fa fa-fw fa-star" />,
   onChange: newValue => {
     console.log(`Example 2: new value is ${newValue}`);
   }
